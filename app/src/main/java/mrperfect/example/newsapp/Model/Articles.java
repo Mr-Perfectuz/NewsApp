062faec4374ed5b34b8fd180cb17ff2e43840272
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-09-24 08:40:24 +0500

    Articles Headlines Sourse have been added

--- app/src/main/java/mrperfect/example/newsapp/Model/Articles.java
+++ app/src/main/java/mrperfect/example/newsapp/Model/Articles.java
@@ -1,4 +1,81 @@
 package mrperfect.example.newsapp.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 class Articles {
+
+    @SerializedName("Sourse")
+    @Expose
+    private String sourse;
+
+    @SerializedName("author")
+    @Expose
+    private Sourse author;
+
+    @SerializedName("title")
+    @Expose
+    private Sourse title;
+
+
+    @SerializedName("description")
+    @Expose
+    private Sourse description;
+
+    @SerializedName("urlToImage")
+    @Expose
+    private Sourse urlToImage;
+
+    @SerializedName("publishedAt")
+    @Expose
+    private Sourse publishedAt;
+
+
+    public String getSourse() {
+        return sourse;
+    }
+
+    public void setSourse(String sourse) {
+        this.sourse = sourse;
+    }
+
+    public Sourse getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Sourse author) {
+        this.author = author;
+    }
+
+    public Sourse getTitle() {
+        return title;
+    }
+
+    public void setTitle(Sourse title) {
+        this.title = title;
+    }
+
+    public Sourse getDescription() {
+        return description;
+    }
+
+    public void setDescription(Sourse description) {
+        this.description = description;
+    }
+
+    public Sourse getUrlToImage() {
+        return urlToImage;
+    }
+
+    public void setUrlToImage(Sourse urlToImage) {
+        this.urlToImage = urlToImage;
+    }
+
+    public Sourse getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(Sourse publishedAt) {
+        this.publishedAt = publishedAt;
+    }
 }
