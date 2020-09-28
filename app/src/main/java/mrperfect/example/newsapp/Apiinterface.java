18675a231a7e27ec4ff4e05d17e58517ebffd9e9
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-09-28 08:36:02 +0500

    Apiinterface has been enabled

--- app/src/main/java/mrperfect/example/newsapp/Apiinterface.java
+++ app/src/main/java/mrperfect/example/newsapp/Apiinterface.java
@@ -1,4 +1,15 @@
 package mrperfect.example.newsapp;
 
+import mrperfect.example.newsapp.Model.Headlines;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 interface Apiinterface {
+
+    @GET("top-headlines")
+    Call<Headlines> getHeadlines(
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+    );
 }
