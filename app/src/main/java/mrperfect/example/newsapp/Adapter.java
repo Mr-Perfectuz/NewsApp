7984e599ee4805ec08372393c830994bc865926c
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-09-28 08:52:26 +0500

    items  has been defined

--- app/src/main/java/mrperfect/example/newsapp/Adapter.java
+++ app/src/main/java/mrperfect/example/newsapp/Adapter.java
@@ -2,8 +2,11 @@ package mrperfect.example.newsapp;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 class Adapter  extends RecyclerView.Adapter<Adapter.ViewHolder> {
@@ -24,8 +27,18 @@ class Adapter  extends RecyclerView.Adapter<Adapter.ViewHolder> {
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle,tvSource,tvDate;
+        ImageView imageView;
+        CardView cardView;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvSource = itemView.findViewById(R.id.tvSource);
+            tvDate = itemView.findViewById(R.id.tvDate);
+            imageView = itemView.findViewById(R.id.image);
+            cardView = itemView.findViewById(R.id.cardView);
         }
     }
 }
